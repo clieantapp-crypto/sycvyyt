f6172079f7498a6c72bc2538b1addb767bd63d27
clieantapp-crypto <clieantapp@gmail.com>
2025-08-12 01:48:33 +0300

    sss

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -10,15 +10,14 @@ import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-  // Your Firebase configuration will be injected here
-  apiKey: "AIzaSyDJ-0cptQFiTZNKOD3YrHmHIjVN9Rk1jSs",
-  authDomain: "clinte-25027.firebaseapp.com",
-  databaseURL: "https://clinte-25027-default-rtdb.firebaseio.com",
-  projectId: "clinte-25027",
-  storageBucket: "clinte-25027.firebasestorage.app",
-  messagingSenderId: "164388154350",
-  appId: "1:164388154350:web:33d2bc724edfe4e0dc3cff",
-  measurementId: "G-XJVRTMJHEV"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
